bd5e16f974e14b5dfbb8b99d36799a4e9d8ff811
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2025-06-22 12:09:15 +0300

    add solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -3,18 +3,43 @@
 describe(`Function 'validateEmail':`, () => {
   const validateEmail = require('./validateEmail');
 
-  it(`should be declared`, () => {
-    expect(validateEmail).toBeInstanceOf(Function);
+  it('should return true for a valid email', () => {
+    expect(validateEmail('my.personal_info-2@mail-1.com')).toBe(true);
   });
 
-  it(`should return boolean`, () => {
+  describe('personal info', () => {
+    it('starts with dot', () => {
+      expect(validateEmail('.test@mail.com')).toBe(false);
+    });
 
+    it('double dots', () => {
+      expect(validateEmail('te..st@mail.com')).toBe(false);
+    });
+
+    it('ends with dot', () => {
+      expect(validateEmail('test.@mail.com')).toBe(false);
+    });
+
+    it('not english letters', () => {
+      expect(validateEmail('тest@mail.com')).toBe(false);
+    });
+
+    it('with special char', () => {
+      expect(validateEmail('test!@mail.com')).toBe(false);
+    });
   });
 
-  it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
-      .toBeTruthy();
+  it('without @', () => {
+    expect(validateEmail('testmail.com')).toBe(false);
   });
 
-  // write more tests here
+  describe('domain', () => {
+    it('starts with dot', () => {
+      expect(validateEmail('test@.mail.com')).toBe(false);
+    });
+
+    it('with special char', () => {
+      expect(validateEmail('test@mail!.com')).toBe(false);
+    });
+  });
 });
